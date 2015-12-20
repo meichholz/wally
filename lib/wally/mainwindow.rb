c689a9a48425d1e26e29a7fce09d21d8f496cfa6
Marian Eichholz <marian.eichholz@freenet.ag>
2015-12-20 23:41:26 +0100

    wip

--- lib/wally/mainwindow.rb
+++ lib/wally/mainwindow.rb
@@ -8,23 +8,37 @@ module Wally
 
     def initialize
       self.class.load_tk
-      TkOption.add '*tearoff', 0
-      @root = TkRoot.new { title 'Wall Viewer app' }
+      @root = TkRoot.new do
+        title 'Wall Viewer app'
+        # geometry '1920x1080+0+0'
+        geometry '1024x800+10+10' if Tk.windowingsystem == 'x11'
+      end
       setup_menu!
     end
 
     def do_frobnify
-      # TODO: find out, when and how the callback is triggered
-      puts "frobbing the foodle"
+      STDERR.puts "frobbing foo and bar"
+    end
+
+    def do_quit
+      STDERR.puts "Quitting"
+      @root.destroy # will end mainloop
     end
 
     def setup_menu!
-      @filemenu = TkMenu.new(@root)
-      @filemenu.add :command, label: 'Frobnify', command: do_frobnify
+      # see http://www.tutorialspoint.com/ruby/ruby_tk_menu.htm
+      # see http://www.tkdocs.com/tutorial/menus.html
+
+      TkOption.add '*tearOff', 0
+      menubar = TkMenu.new(@root)
+
+      file_menu = TkMenu.new(menubar)
+      file_menu.add :command, label: 'Test...', underline: 0, command: Proc.new{ do_frobnify }
+      file_menu.add :separator
+      file_menu.add :command, label: 'Quit', underline: 0, command: Proc.new{ do_quit }
       # connect menus to a bar and the bar to the window
-      @menubar = TkMenu.new(@root)
-      @menubar.add :cascade, menu: @filemenu, label: 'Filefrobnify'
-      @root.menu @menubar
+      menubar.add :cascade, menu: file_menu, label: 'File'
+      @root.menu menubar
     end
 
     def run!
